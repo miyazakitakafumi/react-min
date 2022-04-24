d5991eff359f4b70cadffd35852b8e5a6a359731
unknown <greeeensfreem@gmail.com>
2022-04-24 10:20:22 +0900

    init

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+const name = 'tamibouz';
+
+ReactDOM.render(
+  <App name={name} />,
+  document.getElementById('app')
+);
